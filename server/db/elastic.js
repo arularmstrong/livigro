a47a838ed79305a29d1be7a62bcdcfaadcdc7dea
arul armstrong <33225342+arularmstrong@users.noreply.github.com>
2018-10-30 11:53:14 -0400

    Update elastic.js

--- server/db/elastic.js
+++ server/db/elastic.js
@@ -4,7 +4,7 @@ var bonsaiUrl    = config.bonsai;
 
 
     var client= new elasticsearch.Client({
-                                host: bonsaiUrl,
+                                host: "https://4obkp3061h:6xa2ft2yf6@livigro-4898362821.ap-southeast-2.bonsaisearch.net",
                                // log: 'trace'
                             });
 
